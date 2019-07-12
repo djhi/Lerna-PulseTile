d207eb058e89bd96ca56f5dc66cd10ebe86da98c
BogdanScherban <bsc@piogroup.net>
2019-07-12 18:33:54 +0300

    first

---
+++ projects/showcase/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Core from "pulsetile-react-admin";
+
+class App extends Component {
+  render() {
+    return (
+        <Core />
+    )
+
+  }
+}
+
+export default App;
